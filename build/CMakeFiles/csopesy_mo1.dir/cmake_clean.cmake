82729539355aba1aefc0b491ea3ec056d1e7f72c
PyroRSC <109332014+PyroRSC@users.noreply.github.com>
2026-05-22 13:44:53 +0800

    initial cpp code for mp1

---
+++ build/CMakeFiles/csopesy_mo1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/csopesy_mo1.dir/link.d"
+  "CMakeFiles/csopesy_mo1.dir/src/main.cpp.o"
+  "CMakeFiles/csopesy_mo1.dir/src/main.cpp.o.d"
+  "csopesy_mo1"
+  "csopesy_mo1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/csopesy_mo1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+int main()
+{
+    std::cout << "HEADING";
+
+    // ConsoleManager consoleManager;
+    // consoleManager.setCursorPosition(0, 1);
+}
+
+
+
+// void ConsoleManager::setCursorPosition (int posX, int posY) const
+// {
+//     COORD coord;
+//     coord.X = posX;
+//     coord.Y = posY;
+//     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+// };
